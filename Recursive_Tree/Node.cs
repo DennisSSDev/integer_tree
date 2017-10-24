7f4c302721aeb6696226d62290511562f00c32c2
Dennis-HomePC <dennis.slavinsky@gmail.com>
2017-10-24 04:06:23 -0400

    Starter File

---
+++ Recursive_Tree/Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursive_Tree
+{
+    class Node
+    {
+        public int Data { get { return data; } set { data = value; } }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+        public Node Peer { get; set; }
+        private int data;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+        public int DataValue()
+        {
+            return data;
+        }
+    }
+}
---
+++ Recursive_Tree/Tree.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursive_Tree
+{
+    class Tree
+    {
+        Node root = null;
+        int counter = 1;
+        int value_of_LeftChild = 0;
+        int value_of_RightChild = 0;
+        bool LeftOrRight;//if the node is left it's true, if it's right it's false;
+        public Tree()
+        {
+        }
+
+        public void Insert(int goal)
+        {
+            if(root == null)
+            {
+                root = new Node(1);
+            }
+            Insert(root, goal);
+        }
+        public void Print()
+        {
+            Print(root, 1);
+        }
+
+        private void Insert(Node nodeP, int goal)
+        {
+            while (counter !=goal)
+            {
+                if(nodeP == null)
+                {
+
+                }
+                counter++;
+                Insert(nodeP.Left, goal);
+                Insert(nodeP.Right, goal);
+            }
+            
+        }
+        private void Print(Node node, int level)
+        {
+
+        }
+
+
+
+
+        /*if (nodeP == null)
+            {
+                if (Peer != null)
+                {
+                    nodeP = new Node(dataP + Peer.Data);
+                }
+                else
+                {
+                    nodeP = new Node(dataP);
+                }
+            }
+            if (counter >= goal)
+            {
+                return;
+            }
+            else
+            {
+                counter++;
+            }
+            Insert(nodeP.DataValue(), nodeP.Left, goal, nodeP);
+            Insert(nodeP.DataValue(), nodeP.Right, goal, nodeP);
+            value_of_LeftChild = nodeP.DataValue();
+            value_of_RightChild = nodeP.DataValue();
+            */
+    }
+}
